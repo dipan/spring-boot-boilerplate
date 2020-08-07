e728ebde3dc57a53dc4a0c3ab595c9f3ba6f8d3d
DMandal <50593269+mandaldipan01@users.noreply.github.com>
2020-08-07 18:30:52 +0530

    GET added

--- src/main/java/com/trafficrecorder/trafficrecorder/api/v0/visitor/VisitorController.java
+++ src/main/java/com/trafficrecorder/trafficrecorder/api/v0/visitor/VisitorController.java
@@ -1,21 +1,38 @@
 package com.trafficrecorder.trafficrecorder.api.v0.visitor;
 
-import com.sun.xml.internal.ws.api.message.Header;
 import com.trafficrecorder.trafficrecorder.api.v0.RestControllerV0;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController("/visitor")
 @RequestMapping(RestControllerV0.BASE_URL + "/visitor")
 public class VisitorController extends RestControllerV0 {
     @PostMapping
-    public String postVisitorCount() {
+    public String postVisitor() {
         try {
 //            authoriseUser();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return "Visitor details recorded successfully";
+    }
+
+    @GetMapping
+    public List<String> getVisitor() {
+        List<String> visitorList = new ArrayList<>();
+        try {
+//            authoriseUser();
+            visitorList.add("Visitor 1");
+            visitorList.add("Visitor 2");
+            visitorList.add("Visitor 3");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return visitorList;
     }
 }
